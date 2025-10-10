5d11c93674cd4d067e330229e17e516cf2dde01b
Constantin Avadanei <constantin.avadanei@mendelio.io>
2025-10-10 13:07:35 +0300

    Fixed extend rules nr.2

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,15 @@
+// eslint.config.js
 import js from "@eslint/js";
 import globals from "globals";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
-import tsPlugin from "@typescript-eslint/eslint-plugin";
 import { globalIgnores } from "eslint/config";
+
+const tsRecommendedRules = tsPlugin.configs.recommended.rules;
+const reactHooksRules = reactHooks.configs["recommended-latest"].rules;
+const reactRefreshRules = reactRefresh.configs.vite.rules;
 
 export default [
   globalIgnores(["dist"]),
@@ -11,11 +17,11 @@ export default [
   {
     files: ["**/*.{ts,tsx}"],
     languageOptions: {
-      parser: "@typescript-eslint/parser",
+      parser: tsParser,
       parserOptions: {
-        project: "./tsconfig.app.json",
-        sourceType: "module",
         ecmaVersion: 2022,
+        sourceType: "module",
+        project: "./tsconfig.app.json",
       },
       globals: globals.browser,
     },
@@ -24,12 +30,23 @@ export default [
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
     },
-    // Include rules directly instead of 'extends'
     rules: {
       ...js.configs.recommended.rules,
-      ...tsPlugin.configs.recommended.rules,
-      ...reactHooks.configs["recommended-latest"].rules,
-      ...reactRefresh.configs.vite.rules,
+      ...tsRecommendedRules,
+      ...reactHooksRules,
+      ...reactRefreshRules,
+    },
+  },
+
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: "module",
+      globals: globals.browser,
+    },
+    rules: {
+      ...js.configs.recommended.rules,
     },
   },
 ];
