0f3405c83038328c94d45dcafabd0dcd8ecb7e1b
Constantin Avadanei <constantin.avadanei@mendelio.io>
2025-10-10 12:56:10 +0300

    Fix parser key

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,34 @@
+// eslint.config.js
 import js from "@eslint/js";
 import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
-import tseslint from "typescript-eslint";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
 import { globalIgnores } from "eslint/config";
 
-export default tseslint.config([
+export default [
   globalIgnores(["dist"]),
   {
     files: ["**/*.{ts,tsx}"],
-    parser: "@typescript-eslint/parser",
-    parserOptions: {
-      project: "./tsconfig.app.json",
-      sourceType: "module",
-      ecmaVersion: 2022,
-    },
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs["recommended-latest"],
-      reactRefresh.configs.vite,
-    ],
     languageOptions: {
+      parser: "@typescript-eslint/parser",
+      parserOptions: {
+        project: "./tsconfig.app.json",
+        sourceType: "module",
+        ecmaVersion: 2022,
+      },
       globals: globals.browser,
     },
+    plugins: {
+      "@typescript-eslint": tsPlugin,
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+    },
+    rules: {
+      ...tsPlugin.configs.recommended.rules,
+      ...reactHooks.configs["recommended-latest"].rules,
+      ...reactRefresh.configs.vite.rules,
+    },
+    extends: [js.configs.recommended],
   },
-]);
+];
